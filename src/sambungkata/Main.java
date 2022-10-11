765f7b3c527350818cc7c15bb720ac723eee58a3
MuhamadArifAdiputra <33291898+MuhamadArifAdiputra@users.noreply.github.com>
2022-10-11 14:31:17 +0700

    Update Main.java for basic looping

--- src/sambungkata/Main.java
+++ src/sambungkata/Main.java
@@ -1,7 +1,9 @@
-/**
- * 
- */
 package sambungkata;
+
+/**
+ * Main program untuk permainan sambungkata
+ */
+import java.util.Scanner;  // Import the Scanner class
 
 /**
  * @author MuhamadArifAdiputra
@@ -18,7 +20,24 @@ public class Main {
 		System.out.println("Halo Peserta DevCamp Tokopedia Academy 2022!");
 		
 		System.out.println("Selamat program nya sudah berjalan! Silakan eksplor ya!");
+		
+		boolean stillWantToPlay = true;
+		
+		while (stillWantToPlay) {
+			
+			
+		    Scanner rawInput = new Scanner(System.in);  // Create a Scanner object
+		    System.out.print("Masih mau main? (Y/N) :");
 
+		    String userInput = rawInput.nextLine();  // Read user input
+		    
+		    if (userInput.equals("N")) {
+		    	System.out.println("Okay, sampai jumpa lagi ya...");
+		    	stillWantToPlay = false;
+		    }
+			
+		}
+		
 	}
 
 }
