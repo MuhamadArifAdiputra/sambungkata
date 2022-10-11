bc8680552ffc42d735a42d73e7013e6610c23d4b
MuhamadArifAdiputra <33291898+MuhamadArifAdiputra@users.noreply.github.com>
2022-10-11 14:34:37 +0700

    Minor update

--- src/sambungkata/Main.java
+++ src/sambungkata/Main.java
@@ -1,7 +1,7 @@
 package sambungkata;
 
 /**
- * Main program untuk permainan sambungkata
+ * Main Program for sambungkata game
  */
 import java.util.Scanner;  // Import the Scanner class
 
@@ -18,19 +18,19 @@ public class Main {
 		// TODO Auto-generated method stub
 		// Silakan coba jalankan program ini terlebih dahulu untuk memastikan sudah bisa run dengan baik ya
 		System.out.println("Halo Peserta DevCamp Tokopedia Academy 2022!");
-		
 		System.out.println("Selamat program nya sudah berjalan! Silakan eksplor ya!");
 		
-		boolean stillWantToPlay = true;
 		
+		
+		boolean stillWantToPlay = true;
 		while (stillWantToPlay) {
-			
-			
+			// Get user input for replay
 		    Scanner rawInput = new Scanner(System.in);  // Create a Scanner object
-		    System.out.print("Masih mau main? (Y/N) :");
-
+		    System.out.print("Masih mau main? (Y/N) : ");
 		    String userInput = rawInput.nextLine();  // Read user input
 		    
+		    
+		    // If user input N, then exit
 		    if (userInput.equals("N")) {
 		    	System.out.println("Okay, sampai jumpa lagi ya...");
 		    	stillWantToPlay = false;
