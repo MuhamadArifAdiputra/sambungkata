358259d28eacd5a3f16238c2912fccb4821c0b5b
MuhamadArifAdiputra <33291898+MuhamadArifAdiputra@users.noreply.github.com>
2022-10-11 13:46:16 +0700

    Update Main.java

--- src/sambungkata/Main.java
+++ src/sambungkata/Main.java
@@ -14,7 +14,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Silakan coba jalankan program ini terlebih dahulu untuk memastikan sudah bisa run dengan baik ya
 		System.out.println("Halo Peserta DevCamp Tokopedia Academy 2022!");
+		
+		System.out.println("Selamat program nya sudah berjalan! Silakan eksplor ya!");
 
 	}
 
