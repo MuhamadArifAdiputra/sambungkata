6bdd20a3afa9aa90d4e6c5a271d0f4f2b2320917
Rafael Tauran <73337430+AutisMaxima@users.noreply.github.com>
2022-10-12 09:29:36 +0100

    Update Main.java

--- src/sambungkata/Main.java
+++ src/sambungkata/Main.java
@@ -3,6 +3,65 @@
  */
 package sambungkata;
 
+class CustomPair {
+
+	String[] arr;
+
+	public CustomPair(String s1, String s2) {
+		arr = new String[2];
+		arr[0] = s1;
+		arr[1] = s2;
+	}
+
+	public String sambung() {
+		int y0 = findLastVowel(arr[0]);
+		if (y0 < arr[0].length() - 1)
+			y0 += 1;
+
+		int x0 = y0 - 1;
+		for (int i = x0; i >= 0; i--) {
+			x0 = i;
+			if (arr[0].charAt(i) != 'a' &&
+					arr[0].charAt(i) != 'e' &&
+					arr[0].charAt(i) != 'i' &&
+					arr[0].charAt(i) != 'o' &&
+					arr[0].charAt(i) != 'u')
+				break;
+			else if (i == 0)
+				x0 = -1;
+		}
+
+		if (x0 == -1)
+			return "";
+
+		String substring = arr[0].substring(x0, y0 + 1);
+		System.out.println(substring);
+		if (arr[1].contains(substring) && substring.charAt(0) == arr[1].charAt(0))
+			return substring;
+		else
+			return "";
+
+	}
+
+	public int findLastVowel(String s) {
+		int[] indexes = {
+				s.lastIndexOf('a'),
+				s.lastIndexOf('e'),
+				s.lastIndexOf('i'),
+				s.lastIndexOf('o'),
+				s.lastIndexOf('u')
+		};
+
+		int maxIndex = -1;
+		for (int i = 0; i < indexes.length; i++) {
+			maxIndex = Math.max(indexes[i], maxIndex);
+		}
+
+		return maxIndex;
+	}
+
+}
+
 /**
  * @author MuhamadArifAdiputra
  *
@@ -13,12 +72,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Silakan coba jalankan program ini terlebih dahulu untuk memastikan sudah bisa run dengan baik ya
-		System.out.println("Halo Peserta DevCamp Tokopedia Academy 2022!");
-		
-		System.out.println("Selamat program nya sudah berjalan! Silakan eksplor ya!");
-
+		CustomPair cPair = new CustomPair("changgi", "ganteng");
+		System.out.println("Kata sambungan: " + cPair.sambung());
 	}
 
 }
