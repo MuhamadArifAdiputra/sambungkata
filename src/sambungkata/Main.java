d5677e35fbe3c3606ff14ab94cc2430d877ebfbc
myanpetra99 <57763111+myanpetra99@users.noreply.github.com>
2022-10-12 15:37:32 +0700

    Level 1

--- src/sambungkata/Main.java
+++ src/sambungkata/Main.java
@@ -1,7 +1,6 @@
-/**
- * 
- */
+
 package sambungkata;
+import java.util.Scanner;
 
 /**
  * @author MuhamadArifAdiputra
@@ -12,13 +11,30 @@ public class Main {
 	/**
 	 * @param args
 	 */
+
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Silakan coba jalankan program ini terlebih dahulu untuk memastikan sudah bisa run dengan baik ya
-		System.out.println("Halo Peserta DevCamp Tokopedia Academy 2022!");
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Masukkan kata pertama : ");
+		String kata1 = input.nextLine();
+		System.out.println("Masukkan kata kedua : ");
+		String kata2 = input.nextLine();
+
+		Integer index = 0;
+
+		for(int i = kata1.length() - 2; i > 0; i--) {
+				if ( kata1.charAt(i) != 'A' || kata1.charAt(i) != 'I' || kata1.charAt(i) != 'U' || kata1.charAt(i) != 'E' || kata1.charAt(i) != 'O') {
+					index = i;
+					break;
+				}
+			};
 		
-		System.out.println("Selamat program nya sudah berjalan! Silakan eksplor ya!");
-
+		if ( ((kata1.substring(index, kata1.length())).toLowerCase()).equals((kata2.substring(0, kata1.length() - index)).toLowerCase()) ){
+			System.out.println("Kata pertama dan kedua dapat disambung");
+		} else {
+			System.out.println("Kata pertama dan kedua tidak dapat disambung");
+		};
+		};
 	}
-
-}
